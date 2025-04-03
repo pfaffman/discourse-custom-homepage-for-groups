6b325b86cc499f6fbe897d967d8657b61a55f199
Jay Pfaffman <jay@literatecomputing.com>
2025-04-03 10:55:58 -0500

    remove unused sitesetting

--- javascripts/discourse/initializers/custom-homepage-set.js
+++ javascripts/discourse/initializers/custom-homepage-set.js
@@ -1,32 +1,12 @@
-import mobile from "discourse/lib/mobile";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import PreloadStore from "discourse/lib/preload-store";
+import { setDefaultHomepage } from "discourse/lib/utilities";
 
 export default {
   name: "discourse-custom-homepage",
   initialize() {
     withPluginApi("0.11.4", (api) => {
-      // const router = container.lookup("router:main");
       const user = api.getCurrentUser();
-      const { setDefaultHomepage } = require("discourse/lib/utilities");
-
-      if (settings.custom_default_homepage) {
-        const url = settings.custom_default_homepage.replace(/^\/+/g, "");
-        setDefaultHomepage(url);
-        PreloadStore.remove("topic_list");
-      }
-
-      if (!user && settings.anon_page) {
-        const url = settings.anon_page.replace(/^\/+/g, "");
-        setDefaultHomepage(url);
-        PreloadStore.remove("topic_list");
-      }
-
-      if (mobile.isMobileDevice && settings.mobile_homepage) {
-        const url = settings.mobile_homepage.replace(/^\/+/g, "");
-        setDefaultHomepage(url);
-        PreloadStore.remove("topic_list");
-      }
 
       if (user) {
         if (user.primary_group_name && settings.group_page_map) {
@@ -36,7 +16,6 @@ export default {
           );
           if (mapEntry) {
             const url = mapEntry.split(":")[1].replace(/^\/+/g, "");
-            window.console.log("setting url", url);
             setDefaultHomepage(url);
             PreloadStore.remove("topic_list");
           }
