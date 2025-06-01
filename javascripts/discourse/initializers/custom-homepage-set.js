cd37d8a24d592188cd77dd6fba3a62987a7fb152
Jay Pfaffman <jay@literatecomputing.com>
2025-06-01 11:12:28 -0400

    add back suport for custom homepage and anon homepage

--- javascripts/discourse/initializers/custom-homepage-set.js
+++ javascripts/discourse/initializers/custom-homepage-set.js
@@ -7,6 +7,11 @@ export default {
   initialize() {
     withPluginApi("0.11.4", (api) => {
       const user = api.getCurrentUser();
+      let url = null;
+
+      if (settings.custom_default_homepage) {
+        url = settings.custom_default_homepage.replace(/^\/+/g, "");
+      }
 
       if (user) {
         if (user.primary_group_name && settings.group_page_map) {
@@ -15,11 +20,16 @@ export default {
             RegExp(user.primary_group_name).test(value)
           );
           if (mapEntry) {
-            const url = mapEntry.split(":")[1].replace(/^\/+/g, "");
-            setDefaultHomepage(url);
-            PreloadStore.remove("topic_list");
+            url = mapEntry.split(":")[1].replace(/^\/+/g, "");
           }
         }
+      } else if (settings.anon_page) {
+        url = settings.anon_page.replace(/^\/+/g, "");
+      }
+
+      if (url) {
+        setDefaultHomepage(url);
+        PreloadStore.remove("topic_list");
       }
     });
   },
